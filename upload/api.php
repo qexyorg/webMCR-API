a37dd8d31d363e33f1be8b9140d179f38c2ef00f
Qexy <admin@qexy.org>
2015-07-26 23:54:38 +0300

    Added CSRF security protection

--- upload/api.php
+++ upload/api.php
@@ -17,7 +17,7 @@ require_once(API_DIR_REMOTE."api.class.php");
 
 $api = new api();
 
-if(!isset($_REQUEST['apicsrf'])){ $api->result('Incorrect security key') }
+if(!isset($_REQUEST['apicsrf'])){ $api->result('Incorrect security key'); }
 
 $apicfg = md5($api->getIP().$api->cfg['csrfkey']);
 if($_REQUEST['apicsrf']!==$apicfg){ $api->result('Incorrect security key'); }
